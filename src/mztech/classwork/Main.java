74e47af0ac13e04a319de5f5001d62c8fe1b706d
jarinmahmud <jarinmahmud@gmail.com>
2022-01-15 18:24:29 -0600

    New Commit

--- src/mztech/classwork/Main.java
+++ src/mztech/classwork/Main.java
@@ -13,8 +13,23 @@ public class Main{
 
    System.out.println(""): keyword to print anything
      */
+    //camelCase
+    //PascalCase
     public static void main(String[] args){
         System.out.println("Hello World!");
+        System.out.println(10 -1);
+        System.out.println(4 * 6);
+        System.out.print("Hello");
+        System.out.print("World");
+
+        //System: It is a final class defined in the java.lang package.
+        //out: This is an instance of PrintStream type, which is a public and static member field of the System class.
+        /*
+        println(): As all instances of PrintStream class have a public method println(), hence we can invoke the same on out as well.
+         This is an upgraded version of print().
+        It prints any argument passed to it and adds a new line to the output.
+
+         */
 
     }
 }
