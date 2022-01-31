c51b682957bd93c2de146228591dd545b0349735
jarinmahmud <jarinmahmud@gmail.com>
2022-01-31 02:12:44 -0600

    New Commit

---
+++ src/mztech/classwork/Grandparents.java
@@ -0,0 +1,7 @@
+package mztech.classwork;
+
+public class Grandparents {
+    static String firstName = "Mohammed";
+
+    static String lastname = "BEGUM";
+}
--- src/mztech/classwork/TwoDArray.java
+++ src/mztech/classwork/TwoDArray.java
@@ -3,9 +3,11 @@ package mztech.classwork;
 public class TwoDArray {
     public static void main(String[] args) {
         int[][] arr = {{1, 2}, {3, 4}, {5, 6}};// declaring array
+
         //printing array
         for (int i = 0; i < arr.length; i++) {
             System.out.print("[");
+
             for (int j = 0; j < arr[i].length; j++) {
                 System.out.print(arr[i][j]);
                 int lastIndex = arr[i].length - 1;
