3614e4b5cdf757b2c420d08c2ed8513922fdcfaf
jarinmahmud <jarinmahmud@gmail.com>
2022-02-23 18:06:36 -0600

    02.23.2022

--- src/Homework/InfoInput.java
+++ src/Homework/InfoInput.java
@@ -4,18 +4,27 @@ public class InfoInput {
     String name;
     String email;
     String phone;
-    String address;
+    String streetAddress;
+    String city;
+    String state;
+    int zip;
 
-    public InfoInput(String inputName, String inputEmail, String inputPhone, String inputAddress) {
+    public InfoInput(String inputName, String inputEmail, String inputPhone, String inputAddress, String inputCity, String inputState, int inputZip) {
         name = inputName;
         email = inputEmail;
         phone = inputPhone;
-        address = inputAddress;
+        streetAddress = inputAddress;
+        city = inputCity;
+        state = inputState;
+        zip = inputZip;
 
         System.out.println("Name:" + name);
         System.out.println("Email:" + email);
         System.out.println("Phone:" + phone);
-        System.out.println("Address:" + address);
+        System.out.println("Address:" + streetAddress + ",");
+        System.out.print(city + ",");
+        System.out.print(state + ",");
+        System.out.println(zip + ".");
         System.out.println("\n");
 
 
--- src/Homework/InfoPrint.java
+++ src/Homework/InfoPrint.java
@@ -2,8 +2,8 @@ package Homework;
 
 public class InfoPrint {
     public static void main(String[] args) {
-        InfoInput Jarin = new InfoInput("Jarin Mahmud", "jarinmahmud@gmail.com", "9294004804", "Dallas,Texas");
-        InfoInput John = new InfoInput("John Doe", "sdfsdf@gmail.us", "12356795628", "123 Street 80 Avenue, Jamaica, NY-11432");
+        InfoInput Jarin = new InfoInput("Jarin Mahmud", "jarinmahmud@gmail.com", "9294004804", "7220 MCCallum Blvd", "Dallas", "Texas", 75252);
+        InfoInput John = new InfoInput("John Doe", "sdfsdf@gmail.us", "12356795628", "123 Street 80 Avenue", "Jamaica", "NY", 11435);
 
     }
 }
