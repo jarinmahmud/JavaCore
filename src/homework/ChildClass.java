be762cd2e243d29d93a5f7cf96b91b5fa498b8f3
jarinmahmud <jarinmahmud@gmail.com>
2022-07-07 17:28:26 -0500

    070722

--- src/homework/ChildClass.java
+++ src/homework/ChildClass.java
@@ -3,13 +3,15 @@ package homework;
 public class ChildClass extends ParentClass {
     public static void main(String[] args) {
 
-        ChildClass object = new ChildClass();//it works without creating the object too, why creating object then?for constructor?as far we know child class inherites the parent constructor too...
+        ChildClass object = new ChildClass();//it is an option, specifying
+        //it works without creating the object too, why creating object then?for constructor?as far we know child class inherites the parent constructor too...
 
         System.out.println(lastName);
         object.printFirstName();
 
         ParentClass pobject = new ParentClass();
         ParentClass p = new ChildClass();
+        //It cant be ChildClass c = new ParentClass(); because ParentClass cannot inherit ChildClass.
 
         pobject.favoriteFood();// can be written super.favoritefood(); but super keyword requires non-static method to compile/run.
         p.favoriteFood();
@@ -24,8 +26,3 @@ public class ChildClass extends ParentClass {
 
 
 }
-/*Question on referencing parent class
-Animal a = new Animal();   // Animal reference and object
-Animal b = new Dog();   // Animal reference but Dog object
-//can it be written.... Dog c = new animal(); ? // Dog reference but animal object?
- */
--- src/homework/ParentClass.java
+++ src/homework/ParentClass.java
@@ -2,7 +2,10 @@ package homework;
 
 public class ParentClass {
     static String firstName = "Jarin";
-    static String lastName = "Mahmud"; //why static? why does niot work without static?
+    static String lastName = "Mahmud"; //why static? why does not work without static?
+    //for creating instance
+    // we dont want to have another first name, for that we need to static the variable
+    //it is nit class level, outside main method
 
     public static void printFirstName() {
         System.out.println(firstName);
